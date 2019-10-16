de31cd0c7d8dddad09522189a6bc14094228d8a9
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-10-16 15:35:41 +0000

    Snapshot of branch master taken on 2019-10-16T11:35:40.376-04:00 from "https://github.com/UnofficialJuliaMirror/RankAggregation.jl-ab9d97fe-b3f0-4ed5-83d9-5747a4381b0c"

---
+++ src/RankAggregation.jl
@@ -0,0 +1,42 @@
+# ------------------------------------------------------------------
+# Licensed under the ISC License. See LICENCE in the project root.
+# ------------------------------------------------------------------
+
+module RankAggregation
+
+using Tables
+
+"""
+    AggregationMethod
+
+A rank aggregation method.
+"""
+abstract type AggregationMethod end
+
+"""
+    rank(objects, scores, method; rev=false)
+
+Rank `objects` stored in a tabular format on the basis of
+`scores` columns and with an aggregation `method`.
+"""
+function rank(objects, scores::NTuple{N,Symbol},
+              method::AggregationMethod=TauModel();
+              rev=false) where {N}
+  r = rank_impl(objects, scores, method)
+  rev ? length(r) .- r .+ 1 : r
+end
+
+rank_impl(::Any, ::NTuple{N,Symbol}, ::AggregationMethod) where {N} =
+  @error "not implemented"
+
+#------------------
+# IMPLEMENTATIONS
+#------------------
+include("tau_model.jl")
+
+export
+  AggregationMethod,
+  TauModel,
+  rank
+
+end # module
---
+++ src/tau_model.jl
@@ -0,0 +1,44 @@
+# ------------------------------------------------------------------
+# Licensed under the ISC License. See LICENCE in the project root.
+# ------------------------------------------------------------------
+
+"""
+    TauModel()
+
+Probabilistic rank aggregation with the Tau model.
+
+## References
+
+* Journel 2002. Combining Knowledge From Diverse Sources:
+  An Alternative to Traditional Data Independence Hypotheses.
+"""
+struct TauModel <: AggregationMethod end
+
+function rank_impl(objects, scores::NTuple{N,Symbol},
+                   method::TauModel) where {N}
+  # score columns
+  cols = []
+  for col in propertynames(objects)
+    if col ∈ scores
+      push!(cols, getproperty(objects, col))
+    end
+  end
+
+  # conditional probabilities
+  S = reduce(hcat, cols)
+  P = S ./ sum(S, dims=1)
+  n = size(P, 1)
+
+  # uniform prior
+  xₒ = (1 - 1/n) / (1/n)
+
+  # odds with no redundancy
+  X = (1 .- P) ./ P
+  x = xₒ * prod(X/xₒ, dims=2)
+
+  # posterior probabilities
+  p = 1 ./ (1 .+ x)
+
+  # final rank
+  sortperm(vec(p), rev=true)
+end
